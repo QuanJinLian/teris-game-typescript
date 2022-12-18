cc796e1a37a5c90dc1abb7c61990351dd378a5cb
QuanJinLian <jinlian.quan@atto-research.com>
2022-12-18 17:26:35 +0900

    블록을 화면에 표현하는 viewer 클래스 구현 등

---
+++ src/core/viewer/SquarePageViewer.ts
@@ -0,0 +1,47 @@
+import { Square } from "../Square";
+import $ from "jquery";
+import { IViewer } from "../types";
+import PageConfig from "./PageConfig";
+
+/**
+ *
+ * 테트리스 오브젝트 화면에 표현
+ *
+ */
+
+export class SquarePageViewer implements IViewer {
+  private dom?: JQuery<HTMLElement>;
+  private isRemove: boolean = false;
+
+  constructor(private square: Square, private container: JQuery<HTMLElement>) {}
+
+  show(): void {
+    if (this.isRemove) {
+      return;
+    }
+
+    if (!this.dom) {
+      this.dom = $("<div>")
+        .css({
+          position: "absolute",
+          width: PageConfig.SquareSize.width,
+          height: PageConfig.SquareSize.height,
+          border: "1px soild #ccc",
+          boxSizing: "border-box",
+        })
+        .appendTo(this.container);
+    }
+    this.dom.css({
+      left: this.square.point.x * PageConfig.SquareSize.width,
+      top: this.square.point.y * PageConfig.SquareSize.height,
+      background: this.square.color,
+    });
+  }
+
+  remove(): void {
+    if (this.dom && !this.isRemove) {
+      this.dom?.remove();
+      this.isRemove = true;
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export default "hello";
+import { Square } from "./core/Square";
+import { SquarePageViewer } from "./core/viewer/SquarePageViewer";
+import $ from "jquery";
+
+const sq = new Square();
+sq.viewer = new SquarePageViewer(sq, $("#root"));
+
+sq.color = "red";
+sq.point = {
+  x: 3,
+  y: 0,
+};
+
+$("#btnDown").click(() => {
+  sq.point = {
+    x: sq.point.x,
+    y: sq.point.y + 1,
+  };
+});
+$("#btnRemove").click(() => {
+  sq.viewer?.remove();
+});
+$("#btnAdd").click(() => {
+  sq.viewer = new SquarePageViewer(sq, $("#root"));
+});
